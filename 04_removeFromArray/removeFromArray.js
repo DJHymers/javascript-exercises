6046cdbf45101aa582d93c72528cfd0b33e50ac0
DJHymers <david_hymers@yahoo.com>
2023-09-04 17:33:30 +0100

    Tests completed for section 4, remove a given argument from the given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...params) {
+  const newArr = arr;
 
+  for (let i = 0; i < params.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (arr[j] === params[i]) {
+        newArr.splice([j], 1);
+      }
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
