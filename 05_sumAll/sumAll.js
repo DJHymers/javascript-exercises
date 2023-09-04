b99d7c484f55c29e766a248557176d305d919334
DJHymers <david_hymers@yahoo.com>
2023-09-04 18:09:14 +0100

    Solutions for section 5, sumall function finsihed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (low, high) {
+  let sum = 0;
 
+  if (
+    low < 0 ||
+    high < 0 ||
+    typeof low !== "number" ||
+    typeof high !== "number"
+  ) {
+    return "ERROR";
+  }
+
+  if (low < high) {
+    for (let i = low; i <= high; i++) {
+      sum += i;
+    }
+  } else if (low > high) {
+    for (let j = low; j >= high; j--) {
+      sum += j;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
