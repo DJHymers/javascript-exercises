e5a4e32c6a5a0fd34712609cb1dcbb57bd6200d1
DJHymers <david_hymers@yahoo.com>
2023-09-04 12:15:41 +0100

    Task 3 completed to reverse a string and return it in a function along with passing tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let split = str.split("");
 
+  let revArr = split.reverse();
+
+  return revArr.join("");
 };
 
 // Do not edit below this line
